661864dc0c7764b8318d535b05f062bcda90beb5
YooJin Kim <lily@YooJins-Laptop.local>
2023-09-24 02:38:37 +0900

    added new file to js folder

---
+++ JS/day07/built_in.js
@@ -0,0 +1,12 @@
+
+const str = 'hello';
+str.charAt(4) // o
+str.concat('My Melody') //hello My Melody
+str.indexOf('e') // 1
+
+const date = new Date();
+console.log(date)
+console.log(date.getFullYear()) // 2023
+console.log(date.getMonth()) // 0 ~ 11 
+console.log(date.getDate()) // 23 
+console.log(date.getDay()) // 6 (0: sunday, 6: saturday)
