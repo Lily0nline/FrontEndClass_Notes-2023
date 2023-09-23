6b711fecc9d47fcaa415119ac7a362737a6c5665
YooJin Kim <lily@YooJins-Laptop.local>
2023-09-24 03:10:09 +0900

    changes made to quiz file in js folder

--- JS/day07/quiz.js
+++ JS/day07/quiz.js
@@ -14,19 +14,21 @@ Math.floor(Math.random() * 45 + 1) // 1 ~ 45 -> 특정 범위에 랜덤 정수
 전화번호가리기
 010-1234-1234 => 010-****-1234
 
-
 퀴즈 2번
 로또 번호 마추기
 랜덤 번호 생성 [1~45, 1~45, 1~45, 1~45, 1~45, 1~45, 1~45]
 input [?,?,?,?,?,?,?]
-중복 번호는 있어서 안된
+중복 번호는 있어서 안됨 -> for, --> push[], includes, new Set()
 
 마지막 번호 = 보너스 번호
 로또 번호 3개 -> 5등
 로또 번호 4개 -> 4등
 로또 번호 5개 -> 3등
 로또 번호 5개 + 보너스  -> 2등
-로또 번호 6개 -> 1등
+로또 번호 6개 -> 1등 --> count - 순회 -> ** --> pop, (filter, at(-1))
+                  --> reduce
+
+                  --> switch --> if
 
 
 
@@ -34,3 +36,10 @@ input [?,?,?,?,?,?,?]
 퀴즈 3번(심화)
 로또 여러장 구매해서 맞추기
 */
+
+// quiz 1 answer
+
+const phoneNumber = '010-1234-1234'
+const splitNumber = phoneNumber.split('-')
+splitNumber[1] = "****"
+console.log(splitNumber.join('-'))
