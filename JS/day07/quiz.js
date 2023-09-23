f7b71ceeaba3eff612fa3d251e5a5fac0c8dca86
YooJin Kim <lily@YooJins-Laptop.local>
2023-09-24 04:08:26 +0900

    made changes to quiz file

--- JS/day07/quiz.js
+++ JS/day07/quiz.js
@@ -86,10 +86,80 @@ function generatedLottoNumber(){
     // 중복 검사 -> 현재까지 작성한 숫자의 배열을 순회
 
     const lottoArray = []
-    for(let i = 0; i<7; i++){
+
+    /*for(let i = 0; i<7; i++){
         const random = Math.floor(Math.random() * 45 + 1)
         if(!lottoArray.includes(random)){
             lottoArray.push(random)
         }
+    }*/
+    //요소의 길이가 명확하지 않기 때문에 while 반복문을 사용
+    //초기식과 증감식이 관련이 없다 반복에, 특정 조건만큼 반복
+
+    //new set()
+    //자바스크립트 배열에서 중복 요소 삭제, 중복 요소 허용 x
+
+    while(lottoArray.length < 7){
+        const random = Math.floor(Math.random()* 45 + 1)
+        if (!lottoArray.includes(random)){
+            lottoArray.push(random)
+        }
     }
+    return lottoArray
 }
+
+const lotto = generatedLottoNumber()
+console.log(lotto)
+
+
+//using the arrow function to not forget about it lol
+const lottery = (numberArr) => {
+    //요구 사항 분석
+    //정답
+    //numberArr와 정답의 비교
+    //맞춘 갯수
+    //맞춘 갯수에 따라서 다른 결과
+    //const winningNumberArray = [23,1,2,4,5,3,12]
+    const winningNumberArray = generatedLottoNumber()
+    const bonusNum = winningNumberArray.pop()
+
+    let winningCount;
+    // 사용자가 인자로 전달한 로또번호 중 각 번호가 정답 번호에 포함되어 있으면 ++
+  for(let num of numberArr){
+    if(winningNumberArray.includes(num)){
+      winningCount ++;
+    }
+  }
+
+  let bonusFlag = false;
+  if(numberArr.includes(bonusNum)){
+    bonusFlag = true;
+  }
+  let rank;
+  switch(winningCount){
+    //...
+    case 6:
+        //if(bonusFlag) return rank = 2;
+       //return rank = 1;
+        if(bonusFlag){
+            rank = 2;
+        } else {
+            rank = 1;
+        }
+        break; 
+    case 5:
+        rank = 3;
+        break;
+    case 4:
+        rank = 4;
+        break;
+    case 3:
+        rank = 5;
+        break;
+    default:
+         rank = "Try again next time"
+  }
+  console.log(rank)
+}
+
+lottery(lotto)
