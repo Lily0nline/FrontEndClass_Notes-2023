4ee6dd8e4ac5f30fc6330c2e9e9c77d9a6103ebd
YooJin Kim <lily@YooJins-Laptop.local>
2023-10-08 20:04:16 +0900

    new file added

---
+++ JS/day10/function_types.js
@@ -0,0 +1,19 @@
+
+/* 
+arrow function
+const funcName = (parameter) => { 
+  실행문
+}
+(parameter) => {
+  실행문
+}
+*/
+
+/* 
+일반 함수
+function funcName (parameter) {
+  실행문
+}
+function (parameter){
+}
+*/
