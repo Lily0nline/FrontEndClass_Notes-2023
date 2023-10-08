2aa731607f4d4a2dac5bce1643533672887f9869
YooJin Kim <lily@YooJins-Laptop.local>
2023-10-08 12:28:34 +0900

    changes made to file

--- JS/day10/js/board.js
+++ JS/day10/js/board.js
@@ -9,7 +9,25 @@ const $boardList = document.querySelector('.board-list')
 // eventListener가 없는 경우 
 
 $boardList.innerText = "Hello"
+//text를 포함하는 경우
+$boardList.innerHTML = '<div>Hello</div>'
+//html을 포함하는 경우
+
 // 1. innerText
 // 2. innerHTML
 // 3. append, appendChild
 // 4. insertAdjacentElement
+
+// MockPosts -> [article, article, article] -> join 'article article article'
+const posts = MockPosts.map((post) => 
+`<div class="board-list"> <article class="board-card">
+<h3 class="flex-center">
+${post.title}
+<button>삭제</button>
+</h3> 
+<div class="flex-center">
+${post.content}
+</div>
+</article>`).join('')
+
+$boardList.innerHTML = posts 
